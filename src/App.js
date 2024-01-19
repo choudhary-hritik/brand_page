4ebb47075018ca94f7372770233617d5e7c18f02
Hritik Choudhary <hrtk159@gmail.com>
2024-01-19 17:37:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import HeroSection from "./Components/HeroSection";
+
+import Navigation from "./Components/Navigation";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,16 @@
+export default function Navigation() {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="./brand_logo.png" alt="brand_logo" />
+      </div>
+      <ul>
+        <li>MENU</li>
+        <li>LOCATION</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+}
